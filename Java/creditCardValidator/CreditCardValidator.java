3fe374ccf5c6fcfcecade5b7b39f9ce7d0cee70b
GMAtheCreative <timothywonder01@gmail.com>
2024-08-07 15:53:27 +0100

    done

--- Java/creditCardValidator/CreditCardValidator.java
+++ Java/creditCardValidator/CreditCardValidator.java
@@ -5,16 +5,180 @@ public class CreditCardValidator{
 		Scanner collect = new Scanner(System.in);
 
 		System.out.println("Hello, kindly Enter Card details to verify");
-		long accountNumber = collect.nextLong();
+		String cardNumber = collect.next();
 		
-		int index = 0;
-		for (index = 0; index < accountNumber.length; index++){
+        	int sumOfEvenAndOdd = 0;
+        	int evenSum = 0;
+        	int oddSum = 0;
+        	int index = 0;
+
+		for (index = 0; index < cardNumber.length(); index++){
 			index += 1;
 		}
 		if (index > 16 || index < 13){
-			System.out.println("invalid card number " + index);
+			System.out.println("invalid card number");	
+		}
+		else if (cardNumber.charAt(0) == '4'){
+			System.out.println("Card Type: Visa Card");
+			System.out.println("Credit card Number: " + cardNumber);
+			System.out.println("Credit Card Digit Length: " + index);
+
 			
+			long numberCardNumber = Long.parseLong(cardNumber);
+			while (numberCardNumber != 0) {
+				long newNumber = numberCardNumber % 10;
+            			numberCardNumber /= 10;
+            			index++;
+
+				if (index % 2 == 0) {
+                			int digit = (int) newNumber * 2;
+					if (digit > 9) {
+                    				int value1 = digit / 10;
+                    				int value2 = digit % 10;
+                    				evenSum += value1 + value2;
+                			}
+ 
+					else {
+                    				evenSum += digit;
+                			}
+				} 
+
+				else {
+                			int oddDigit = (int) newNumber;
+                			oddSum += oddDigit;
+				}
+        		}
+
+			sumOfEvenAndOdd = evenSum + oddSum;
+			if(sumOfEvenAndOdd % 10 == 0){
+				System.out.println("Credit Card Validity Status: Valid");
+			}
+			else{
+        			System.out.println("Credit Card Validity Status: invalid");
+			}
+		}
+
+		else if(cardNumber.charAt(0) == '5'){
+			System.out.println("Card Type: Master Card");
+			System.out.println("Credit card Number: " + cardNumber);
+			System.out.println("Credit Card Digit Length: " + index);
+
+			long numberCardNumber = Long.parseLong(cardNumber);
+			while (numberCardNumber != 0) {
+				long newNumber = numberCardNumber % 10;
+            			numberCardNumber /= 10;
+            			index++;
+
+				if (index % 2 == 0) {
+                			int digit = (int) newNumber * 2;
+					if (digit > 9) {
+                    				int value1 = digit / 10;
+                    				int value2 = digit % 10;
+                    				evenSum += value1 + value2;
+                			}
+ 
+					else {
+                    				evenSum += digit;
+                			}
+				} 
+
+				else {
+                			int oddDigit = (int) newNumber;
+                			oddSum += oddDigit;
+				}
+        		}
+
+			sumOfEvenAndOdd = evenSum + oddSum;
+			if(sumOfEvenAndOdd % 10 == 0){
+				System.out.println("Credit Card Validity Status: Valid");
+			}
+			else{
+        			System.out.println("Credit Card Validity Status: invalid");
+			}
 		}
 		
+		else if(cardNumber.charAt(0) == '6'){
+			System.out.println("Card Type: Discover Card");
+			System.out.println("Credit card Number: " + cardNumber);
+			System.out.println("Credit Card Digit Length: " + index);
+
+			long numberCardNumber = Long.parseLong(cardNumber);
+			while (numberCardNumber != 0) {
+				long newNumber = numberCardNumber % 10;
+            			numberCardNumber /= 10;
+            			index++;
+
+				if (index % 2 == 0) {
+                			int digit = (int) newNumber * 2;
+					if (digit > 9) {
+                    				int value1 = digit / 10;
+                    				int value2 = digit % 10;
+                    				evenSum += value1 + value2;
+                			}
+ 
+					else {
+                    				evenSum += digit;
+                			}
+				} 
+
+				else {
+                			int oddDigit = (int) newNumber;
+                			oddSum += oddDigit;
+				}
+        		}
+
+			sumOfEvenAndOdd = evenSum + oddSum;
+			if(sumOfEvenAndOdd % 10 == 0){
+				System.out.println("Credit Card Validity Status: Valid");
+			}
+			else{
+        			System.out.println("Credit Card Validity Status: invalid");
+			}
+		}
+		
+		else if(cardNumber.charAt(0) == '3' && cardNumber.charAt(1) == '7'){
+			System.out.println("Card Type: American Express card");
+			System.out.println("Credit card Number: " + cardNumber);
+			System.out.println("Credit Card Digit Length: " + index);
+
+			long numberCardNumber = Long.parseLong(cardNumber);
+			while (numberCardNumber != 0) {
+				long newNumber = numberCardNumber % 10;
+            			numberCardNumber /= 10;
+            			index++;
+
+				if (index % 2 == 0) {
+                			int digit = (int) newNumber * 2;
+					if (digit > 9) {
+                    				int value1 = digit / 10;
+                    				int value2 = digit % 10;
+                    				evenSum += value1 + value2;
+                			}
+ 
+					else {
+                    				evenSum += digit;
+                			}
+				} 
+
+				else {
+                			int oddDigit = (int) newNumber;
+                			oddSum += oddDigit;
+				}
+        		}
+
+			sumOfEvenAndOdd = evenSum + oddSum;
+			if(sumOfEvenAndOdd % 10 == 0){
+				System.out.println("Credit Card Validity Status: Valid");
+			}
+			else{
+        			System.out.println("Credit Card Validity Status: invalid");
+			}
+		}
+		
+		else{
+			System.out.println("Your card is not a valid card");
+			System.out.println("Credit card Number: " + cardNumber);
+			System.out.println("Credit Card Digit Length: " + index);
+		}
 	}
 }
